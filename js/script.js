63583ef777d5ecf54cc53456c76431192bff1846
hasanalit <tuxtayevhasan72@gmail.com>
2022-04-28 22:02:56 +0500

    full-js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let elProducts = document.querySelector('.production__list')
+let elClient = document.querySelector('.client__list')
+let elTrusted = document.querySelector('.trusted__list')
 
 
 const getProductsData = function(products, elements){
@@ -42,21 +44,78 @@ const getBarakaProductData = function(link){
 getBarakaProductData('products/')
 
 
-const getBarakaQuestionData = function(link){
-    fetch(`https://alibaraka.pythonanywhere.com/api/${link}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
 
+const getStoryData = function(link, element){
+    link.forEach(res => {
+
+        let newLi = document.createElement('li')
+        let newDesc = document.createElement('img')
+        let newHeading = document.createElement('h4')
+        let newText = document.createElement('p')
+        let newLink = document.createElement('a')
+
+        newLi.setAttribute('class', 'client__item')
+        newDesc.setAttribute('class', 'client__spans')
+        newDesc.setAttribute('src', res.image)
+        newHeading.setAttribute('class', 'client__list-heading')
+        newText.setAttribute('class', 'client__list-desc')
+        newLink.setAttribute('class', 'client__list-link')
+
+
+        newHeading.textContent = res.name
+        newText.textContent = res.text
+
+
+        element.appendChild(newLi)
+        newLi.appendChild(newDesc)
+        newLi.appendChild(newHeading)
+        newLi.appendChild(newText)
+        newLi.appendChild(newLink)
+    })
 }
-
-getBarakaQuestionData('questions/')
 
 
 const getBarakaStoriesData = function(link){
     fetch(`https://alibaraka.pythonanywhere.com/api/${link}`)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => getStoryData(data, elClient));
 
 }
 
 getBarakaStoriesData('stories/')
+
+
+
+
+const getQuestionData = function(link, element){
+    link.forEach(res => {
+        
+        let newLi = document.createElement('li')
+        let newHeading = document.createElement('h3')
+        let newText = document.createElement('p')
+
+        newLi.setAttribute('class', 'trusted__item')
+        newHeading.setAttribute('class', 'trusted__textt')
+        newText.setAttribute('class', 'trusted__descc')
+
+        newLi.textContent = res.question
+        newText.textContent = res.answer
+
+
+
+        element.appendChild(newLi)
+        newLi.appendChild(newHeading)
+        newLi.appendChild(newText)
+
+    })
+}
+
+
+const getBarakaQuestionData = function(link){
+    fetch(`https://alibaraka.pythonanywhere.com/api/${link}`)
+    .then(response => response.json())
+    .then(data => getQuestionData(data, elTrusted));
+
+}
+
+getBarakaQuestionData('questions/')
